abe9ae708ef149596b33be934455326f0e04f86a
arwumax31@gmail.com <arwumax31@gmai.com>
2016-04-07 11:34:56 +0800

    first commit

---
+++ Pods/Target Support Files/Pods-Carousell_demo_collectionView/Pods-Carousell_demo_collectionView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Carousell_demo_collectionView : NSObject
+@end
+@implementation PodsDummy_Pods_Carousell_demo_collectionView
+@end
---
+++ Pods/Target Support Files/Pods-Carousell_demo_collectionView/Pods-Carousell_demo_collectionView-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Carousell_demo_collectionViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Carousell_demo_collectionViewVersionString[];
+
