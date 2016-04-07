40c872309ea4bee8136ebccb700ca5f0ed6f598d
arwumax31@gmail.com <arwumax31@gmai.com>
2016-04-07 10:28:10 +0800

    Initial Commit

---
+++ Carousell_demo_collectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carousell_demo_collectionView
+//
+//  Created by WuKwok Ho on 7/4/2016.
+//  Copyright © 2016 Wu Kwok Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
